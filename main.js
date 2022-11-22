ae15e934368e82cf3281a6db1b61d56faa9c67e7
Bluebender <olliviev@gmail.com>
2022-11-22 14:01:50 +0000

    Troisième sauvegarde

--- main.js
+++ main.js
@@ -1,75 +1,139 @@
-let section1 = document.getElementById("section1");
-let section2 = document.getElementById("section2");
-let section3 = document.getElementById("section3");
-let bouton1 = document.getElementById("bouton1");
-let bouton2 = document.getElementById("bouton2");
-let bouton3 = document.getElementById("bouton3");
-// let input = document.querySelectorAll("section:first-of-type input");
-// let input2 = document.querySelectorAll("section:nth-of-type(2) input");
-// let input3 = document.querySelectorAll("section:last-of-type input");
+// let section1 = document.getElementById("section1");
+// let section2 = document.getElementById("section2");
+// let section3 = document.getElementById("section3");
+// let bouton1 = document.getElementById("bouton1");
+// let bouton2 = document.getElementById("bouton2");
+// let bouton3 = document.getElementById("bouton3");
+// // let input = document.querySelectorAll("section:first-of-type input");
+// // let input2 = document.querySelectorAll("section:nth-of-type(2) input");
+// // let input3 = document.querySelectorAll("section:last-of-type input");
 
 
-bouton1.addEventListener("click", function(){
-    section1.classList.add("passive");
-    section2.classList.remove("passive");
-    section3.classList.add("passive");
-    bouton1.setAttribute("disabled", "true");
-    // for (let i=0; i<input.length; i++){
-    //     input[i].setAttribute("disabled", "true");
-    // }
+// bouton1.addEventListener("click", function(){
+//     section1.classList.add("passive");
+//     section2.classList.remove("passive");
+//     section3.classList.add("passive");
+//     bouton1.setAttribute("disabled", "true");
+//     // for (let i=0; i<input.length; i++){
+//     //     input[i].setAttribute("disabled", "true");
+//     // }
     
-});
+// });
 
-bouton2.addEventListener("click", function(){
-    section1.classList.add("passive");
-    section2.classList.add("passive");
-    section3.classList.remove("passive");
-    bouton2.setAttribute("disabled", "true");
-    // for (let i=0; i<input2.length; i++){
-    //     input2[i].setAttribute("disabled", "true");
-    // }
-});
+// bouton2.addEventListener("click", function(){
+//     section1.classList.add("passive");
+//     section2.classList.add("passive");
+//     section3.classList.remove("passive");
+//     bouton2.setAttribute("disabled", "true");
+//     // for (let i=0; i<input2.length; i++){
+//     //     input2[i].setAttribute("disabled", "true");
+//     // }
+// });
 
-bouton3.addEventListener("click", function(){
-    section1.classList.remove("passive");
-    section2.classList.add("passive");
-    section3.classList.add("passive");
-    bouton3.setAttribute("disabled", "true");
-    // for (let i=0; i<input3.length; i++){
-    //     input3[i].setAttribute("disabled", "true");
-    // }
-});
+// bouton3.addEventListener("click", function(){
+//     section1.classList.remove("passive");
+//     section2.classList.add("passive");
+//     section3.classList.add("passive");
+//     bouton3.setAttribute("disabled", "true");
+//     // for (let i=0; i<input3.length; i++){
+//     //     input3[i].setAttribute("disabled", "true");
+//     // }
+// });
 
+let formOk = "true"
+
+// // On déclare le premier formulaire
 let form1 = document.querySelector("form:first-of-type");
-console.log(form1);
 
 form1.addEventListener("submit", function(event) {
+    // On arrête la soumission
     event.preventDefault();
-    let input1 = document.querySelector("form:first-of-type input:first-of-type");
-    if (input1.value !==""){
-        event.target.submit();
-    }
-    else{
+    // On déclare le premier input
+    let input1 = document.querySelector("#user_name");
+    let input2 = document.querySelector("#mail");
+    let input3 = document.querySelector("#password");
+    let input4 = document.querySelector("#confirmation_password");
+    // Si la valeur de l'input est différente de vide
+    if (input1.value ===""){
+        console.log("On est dans le if")
         // On cré la balise p
         let inputError = document.createElement("p");
         // On déclare le champs filedset 1
         let fieldset = document.querySelector("form:first-of-type fieldset:first-of-type");
         // On rajoute le message d'erreur dans la balise p
-        inputError.innerHTML = "Veuillez choisir un nom d'utilisateur";
+        inputError.innerHTML = "Veuillez entrer un nom d'utilisateur";
         // On ajoute la classe erreur à la balise p
         inputError.classList.add("erreur");
+        // On ajoute la classe erreur à la balise input
         input1.classList.add(("erreur2"))
         // On rajoute le paragraphe p à la fin du premier champ fieldset
         fieldset.appendChild(inputError);
     }
+    else if (input2.value ===""){
+        console.log("On est dans le Else if")
+        // On cré la balise p
+        let inputError2 = document.createElement("p");
+        // On déclare le champs filedset 2
+        let fieldset2 = document.querySelector("form:first-of-type fieldset:nth-of-type(2)");
+        // On rajoute le message d'erreur dans la balise p
+        inputError2.innerHTML = "Veuillez entrer un Email";
+        // On ajoute la classe erreur à la balise p
+        inputError2.classList.add("erreur");
+        // On ajoute la classe erreur à la balise input
+        input2.classList.add(("erreur2"))
+        // On rajoute le paragraphe p à la fin du premier champ fieldset
+        fieldset2.appendChild(inputError2);
+    }
+    else if (input2.value ===""){
+        console.log("On est dans le Else if")
+        // On cré la balise p
+        let inputError2 = document.createElement("p");
+        // On déclare le champs filedset 2
+        let fieldset2 = document.querySelector("form:first-of-type fieldset:nth-of-type(2)");
+        // On rajoute le message d'erreur dans la balise p
+        inputError2.innerHTML = "Veuillez entrer un Email";
+        // On ajoute la classe erreur à la balise p
+        inputError2.classList.add("erreur");
+        // On ajoute la classe erreur à la balise input
+        input2.classList.add(("erreur2"))
+        // On rajoute le paragraphe p à la fin du premier champ fieldset
+        fieldset2.appendChild(inputError2);
+    }
+    else if (input2.value.indexOf("@"){
+        console.log("On est dans le second Else if")
+        // On cré la balise p
+        let inputError2 = document.createElement("p");
+        // On déclare le champs filedset 2
+        let fieldset2 = document.querySelector("form:first-of-type fieldset:nth-of-type(2)");
+        // On rajoute le message d'erreur dans la balise p
+        inputError2.innerHTML = "Veuillez entrer un Email";
+        // On ajoute la classe erreur à la balise p
+        inputError2.classList.add("erreur");
+        // On ajoute la classe erreur à la balise input
+        input2.classList.add(("erreur2"))
+        // On rajoute le paragraphe p à la fin du premier champ fieldset
+        fieldset2.appendChild(inputError2);
+    else{
+        // On relance la soumission
+        event.target.submit();
+    else{
+        // On relance la soumission
+        event.target.submit();
+    }
+    
 });
 
-// let form2 = document.querySelector("form:nth-of-type(2)");
+// // On déclare le premier formulaire
+// let form1 = document.querySelector("form:first-of-type");
 
-// form2.addEventListener("submit", function(event) {
+// form1.addEventListener("submit", function(event) {
+//     // On arrête la soumission
 //     event.preventDefault();
-//     let input2 = document.querySelector("form:first-of-type input:first-of-type");
+//     // On déclare le premier input
+//     let input1 = document.querySelector("form:first-of-type input:first-of-type");
+//     // Si la valeur de l'input est différente de vide
 //     if (input1.value !==""){
+//         // On relance la soumission
 //         event.target.submit();
 //     }
 //     else{
@@ -81,6 +145,8 @@ form1.addEventListener("submit", function(event) {
 //         inputError.innerHTML = "Veuillez choisir un nom d'utilisateur";
 //         // On ajoute la classe erreur à la balise p
 //         inputError.classList.add("erreur");
+//         // On ajoute la classe erreur à la balise input
+//         input1.classList.add(("erreur2"))
 //         // On rajoute le paragraphe p à la fin du premier champ fieldset
 //         fieldset.appendChild(inputError);
 //     }
